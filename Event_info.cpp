dbff8355432071363a58d690e7ce5a010318d248
Katelyn Gillette <katelynogillette@gmail.com>
2025-11-17 16:09:27 -0900

    Update Event_info.cpp

--- Event_info.cpp
+++ Event_info.cpp
@@ -1,18 +1,21 @@
 #include "Event_info.h"
+#include <iostream>
 
 Event_info::Event_info()
 {
+    //id = date in month/day/year with last digit 0 if only event on that date, 1 if second etc
+    id = "0000000";
     title = "";
-    //Date = month/day/year
-    date = 000000;
+    date = "000000";
     start_time = "";
     end_time = "";
     location = "";
     tag = "";
 }
 
-Event_info::Event_info(string t, int d, string start, string end, string l, string event_tag)
+Event_info::Event_info(string event_id, string t, string d, string start, string end, string l, string event_tag)
 {
+    id = event_id;
     title = t;
     date = d;
     start_time = start;
@@ -21,12 +24,17 @@ Event_info::Event_info(string t, int d, string start, string end, string l, stri
     tag = event_tag;
 }
 
+string Event_info::get_id()
+{
+    return id;
+}
+
 string Event_info::get_title()
 {
     return title;
 }
 
-int Event_info::get_date()
+string Event_info::get_date()
 {
     return date;
 }
@@ -49,4 +57,42 @@ string Event_info::get_location()
 string Event_info::get_tag()
 {
     return tag;
+}
+
+void Event_info::print()
+{
+    cout << id << " " << title << " " << date << " " << start_time << " " << end_time << " " << location << " " << tag << endl;
+    return;
+}
+
+bool Event_info::operator<(const Event_info& other) const
+{
+    string a_year = this->date.substr(4, 2);
+    string b_year = other.date.substr(4, 2);
+    string a_mon = this->date.substr(0, 2);
+    string b_mon = other.date.substr(0, 2);
+    string a_day = this->date.substr(2, 2);
+    string b_day = other.date.substr(2, 2);
+    if(this->date == other.date){
+        if(this->start_time == other.start_time){
+            return this->title > other.title;
+        }
+        else{
+            return this->start_time > other.start_time;
+        }
+
+    }
+    else {
+        if(a_year == b_year){
+            if(a_mon == b_mon){
+                return a_day > b_day;
+            }
+            else{
+                return a_mon > b_mon;
+            }
+        }
+        else{
+            return a_year > b_year;
+        }
+    }
 }
